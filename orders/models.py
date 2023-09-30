61164e1801d3a22e64a4fe15767bc09c5b2fb769
AkWinchester <arsenijkiselev03@gmail.com>
2023-09-30 20:00:19 +0300

    first commit

---
+++ orders/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from customers.models import Customer
+
+from robots.models import Robot
+
+
+class Order(models.Model):
+    STATUSES = (
+        ('available', 'В наличии'),
+        ('waiting', 'В ожидании')
+    )
+    robot_serial = models.CharField(max_length=5,blank=False, null=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUSES, default='waiting')
+    notified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.robot} for {self.customer}"
